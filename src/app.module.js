ad556eba0783e7ec61662a160862292c270633c5
happy-Mary <mariashklovska1991@gmail.com>
2017-08-28 15:00:16 +0300

    add uploading files from json

--- src/app.module.js
+++ src/app.module.js
@@ -5,5 +5,7 @@ import './sass/style.scss';
 import appModule from './app/app.module';
 require('./app/app.controller');
 require('./app/list/list.controller');
+require('./app/listgroup/listgroup.controller');
+require('./app/todo/todo.controller');
 
 angular.module('todoModule', [appModule.name]);
