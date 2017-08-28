98c6e2c312d7be461e9c15d889469a7c5dc533cb
MariaChernyak <mchernyak100@gmail.com>
2017-08-28 16:55:02 +0300

    update

--- src/app/list/list.controller.js
+++ src/app/list/list.controller.js
@@ -1,20 +1,14 @@
 import listModule from './list.module';
 
-import data from '../../app-data/lists.json';
+//import data from '../../app-data/lists.json';
 
 
 export default listModule
     .controller('listController', function listController($http) {
         let self = this;
-<<<<<<< HEAD
-        self.helloList = "it's list controller";
-        self.lists = data;
-        console.log(data);
-=======
 
        	$http({method: 'GET', url: 'app-data/lists.json'})
 			.then(function success(response) {
 	                self.lists = response.data;
 	      	});
->>>>>>> MariaChernyak
     });
