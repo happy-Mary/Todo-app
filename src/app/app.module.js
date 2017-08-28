be550444cd2ec42f6c7a02b4c0681259aa5d11ee
Maria Shklovskaya <MariaShklovskaya@coherentsolutions.com>
2017-08-28 18:26:25 +0300

    json conflict

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,3 +1,4 @@
 import ngStorage from 'ngstorage';
 
-export default angular.module('appModule', [ngStorage.name, 'todoModule', 'listModule', 'listGroupModule']);
+// export default angular.module('appModule', [ngStorage.name, 'todoModule', 'listModule', 'listGroupModule']);
+export default angular.module('appModule', [ngStorage.name, 'listModule']);
--- src/app/list/list.controller.js
+++ src/app/list/list.controller.js
@@ -1,14 +1,17 @@
 import listModule from './list.module';
 
-//import data from '../../app-data/lists.json';
+
+
 
 
 export default listModule
     .controller('listController', function listController($http) {
-        let self = this;
-
-       	$http({method: 'GET', url: 'app-data/lists.json'})
-			.then(function success(response) {
-	                self.lists = response.data;
-	      	});
+		let self = this;
+		let data = require('../../app-data/lists.json');
+		console.log(data);
+		self.list = 'list';
+       	// $http({method: 'GET', url: 'app-data/lists.json'})
+		// 	.then(function success(response) {
+	    //             self.lists = response.data;
+	    //   	});
     });
