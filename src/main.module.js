18abe86ddd532bf64410baaafb8c54372f9a67ca
happy-Mary <mariashklovska1991@gmail.com>
2017-08-28 18:26:25 +0300

    json conflict

--- src/main.module.js
+++ src/main.module.js
@@ -5,12 +5,7 @@ import './sass/style.scss';
 import appModule from './app/app.module';
 require('./app/app.controller');
 require('./app/list/list.controller');
-require('./app/todo/todo.controller');
-require('./app/listgroup/listgroup.controller');
-
-
-
-// import data from './app-data/lists.json';
-// console.log(data);
+// require('./app/todo/todo.controller');
+// require('./app/listgroup/listgroup.controller');
 
 angular.module('todoModule', [appModule.name]);
