9eb6009eb3f74966433f349a13f26ca23a4c24d3
MariaChernyak <mchernyak100@gmail.com>
2017-08-28 16:38:03 +0300

    update todo controller

--- src/app.module.js
+++ src/app.module.js
@@ -5,7 +5,9 @@ import './sass/style.scss';
 import appModule from './app/app.module';
 require('./app/app.controller');
 require('./app/list/list.controller');
-require('./app/listgroup/listgroup.controller');
 require('./app/todo/todo.controller');
+require('./app/listgroup/listgroup.controller');
+
+
 
 angular.module('todoModule', [appModule.name]);
