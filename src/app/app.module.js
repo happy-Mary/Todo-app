d93535053010a9d147728062a7e172be254c7160
happy-Mary <mariashklovska1991@gmail.com>
2017-08-28 16:38:03 +0300

    update todo controller

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,3 +1,3 @@
 import ngStorage from 'ngstorage';
 
-export default angular.module('appModule', [ngStorage.name, 'listModule', 'listGroupModule', 'todoModule']);
+export default angular.module('appModule', [ngStorage.name, 'todoModule', 'listModule', 'listGroupModule']);
