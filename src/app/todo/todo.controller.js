d668588ff839a3aaa51f76695619e969a4c23a8d
MariaChernyak <mchernyak100@gmail.com>
2017-08-28 15:00:16 +0300

    add uploading files from json

---
+++ src/app/todo/todo.controller.js
@@ -0,0 +1,12 @@
+import todoModule from './todo.module';
+
+export default todoModule
+	.controller('todoController', function todoController($http){
+		var self = this;
+
+		$http({method: 'GET', url: 'app-data/todo.json'})
+			.then(function success(response) {
+	                self.todo = response.data;
+	      	});
+})
+
