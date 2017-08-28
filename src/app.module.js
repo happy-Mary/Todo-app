6c7c819ff8631b2d044bc4888041f94e6020c595
happy-Mary <mariashklovska1991@gmail.com>
2017-08-28 11:54:06 +0300

    testing main controller

--- src/app.module.js
+++ src/app.module.js
@@ -4,5 +4,6 @@ import './sass/style.scss';
 
 import appModule from './app/app.module';
 require('./app/app.controller');
+require('./app/list/list.controller');
 
 angular.module('todoModule', [appModule.name]);
