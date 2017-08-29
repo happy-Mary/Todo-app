0ba9cce8a98136e06e3f69debe913d83f3f54a6f
MariaChernyak <mchernyak100@gmail.com>
2017-08-29 11:14:33 +0300

    update link

--- src/app/list/list.controller.js
+++ src/app/list/list.controller.js
@@ -4,7 +4,7 @@ export default listModule
     .controller('listController', function listController($http) {
         let self = this;
 
-       	$http({method: 'GET', url: 'app-data/lists.json'})
+       	$http({method: 'GET', url: 'https://api.myjson.com/bins/eigzx'})
 			.then(function success(response) {
 	                self.lists = response.data;
 	      	});
