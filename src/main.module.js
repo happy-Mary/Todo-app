0dd729693cb45d3232e54766e95d40a1ec62a681
MariaChernyak <mchernyak100@gmail.com>
2017-08-29 17:18:31 +0300

    add listgroup component

--- src/main.module.js
+++ src/main.module.js
@@ -5,8 +5,10 @@ import './sass/style.scss';
 import mainModule from './app/app.module';
 require('./app/app.controller');
 require('./app/listgroup/listgroup.controller');
+require('./app/listgroup/listgroup.component');
 require('./app/list/list.controller');
 require('./app/todo/todo.controller');
+
 
 /* this module goes to html ng-app */
 angular.module('appModule', [mainModule.name]);
