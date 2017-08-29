5e3277a21456f7b3654c4e7b9c3d3ca9a92cc31d
happy-Mary <mariashklovska1991@gmail.com>
2017-08-29 11:14:33 +0300

    update link

--- src/app/todo/todo.controller.js
+++ src/app/todo/todo.controller.js
@@ -1,12 +1,12 @@
 import todoModule2 from './todo.module';
 
 export default todoModule2
-	.controller('todoController', function todoController(){
+	.controller('todoController', function todoController($http){
 		var self = this;
 
-		/*$http({method: 'GET', url: 'app-data/todo.json'})
+		$http({method: 'GET', url: 'https://api.myjson.com/bins/1cg6h9'})
 			.then(function success(response) {
 	                self.todo = response.data;
-	      	}); */
+	      	}); 
 });
 
