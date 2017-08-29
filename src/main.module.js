a88d3ba19d8e1ffafa0c4d03d18b56ff989df02f
happy-Mary <mariashklovska1991@gmail.com>
2017-08-29 17:15:08 +0300

    list-task/feature: component for lists ready

--- src/main.module.js
+++ src/main.module.js
@@ -6,6 +6,7 @@ import mainModule from './app/app.module';
 require('./app/app.controller');
 require('./app/listgroup/listgroup.controller');
 require('./app/list/list.controller');
+require('./app/list/list.component');
 require('./app/todo/todo.controller');
 
 /* this module goes to html ng-app */
