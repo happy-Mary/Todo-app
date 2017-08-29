84eb6727294b775e647939fb8d7bdc46442c1698
MariaChernyak <mchernyak100@gmail.com>
2017-08-29 18:42:16 +0300

    add check data

--- src/app/list/list.controller.js
+++ src/app/list/list.controller.js
@@ -1,16 +1,24 @@
 import listModule from './list.module';
 import { URLS } from '../constants';
+import localStorageService from '../app.service';
 
 export default listModule
-    .controller('listController', function listController($http) {
+    .controller('listController', function listController($http, localStorageService) {
         let self = this;
 
-        $http({ method: 'GET', url: URLS.listURL })
-            .then(function successCallback(response) {
-                self.lists = response.data;
-            }, function errorCallback() {
-                console.log("request FAILED");
-                self.lists = require('../../app-data/lists.json');
-            });
+        if(localStorageService.get('lists')){
+            self.listGroups = localStorageService.get('lists');
+        }
+        else{
+	        $http({ method: 'GET', url: URLS.listURL })
+	            .then(function successCallback(response) {
+	                self.lists = response.data;
+	                localStorageService.set('lists', self.lists);
+	            }, function errorCallback() {
+	                // console.log("request FAILED");
+	                // self.lists = require('../../app-data/lists.json');
+	                self.lists = [];
+	            });
+        }
 
     });
