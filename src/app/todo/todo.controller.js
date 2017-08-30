a198598656984af4df311821ae3812a0289d58b3
happy-Mary <mariashklovska1991@gmail.com>
2017-08-30 11:09:12 +0300

    karma-task/feature: changed promises in http

--- src/app/todo/todo.controller.js
+++ src/app/todo/todo.controller.js
@@ -16,17 +16,17 @@ export default todoModule
         if(localStorageService.get('todo')){
             self.listGroups = localStorageService.get('todo');
         }
-        else{
+        else {
             $http({ method: 'GET', url: URLS.todoURL })
-                .then(function successCallback(response) {
-                    self.todo = response.data;
-                    localStorageService.set('todo', self.todo);
-                    localStorageService.set('lists', self.todo);
-                }, function errorCallback() {
-                    // console.log("request FAILED");
-                    // self.todo = require('../../app-data/todo.json');
-                    self.todo = [];
-                });
+            .then(function successCallback(response) {
+                self.todo = response.data;
+                localStorageService.set('todo', self.todo);
+                localStorageService.set('lists', self.todo);
+            })
+            .catch(function errorCallback() {
+                // self.todo = require('../../app-data/todo.json');
+                self.todo = [];
+            });
         }
               
 
