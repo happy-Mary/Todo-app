011b5fe172abd2036dc319e889977413cce1b30c
MariaChernyak <mchernyak100@gmail.com>
2017-08-31 11:58:18 +0300

    add generate id

--- src/app/todo/todo.constructor.js
+++ src/app/todo/todo.constructor.js
@@ -1,5 +1,7 @@
+import generateId from '../module/generateId';
+
 export default function ToDo(id, title, listId) {
- 	this.id = id;
+ 	this.id = generateId();
  	this.title = title;
  	this.listId = listId;
  	this.subtasks = [];
