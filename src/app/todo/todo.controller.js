eb85d86c9cbef8fd54e59d993eb482c8c8fe7054
happy-Mary <mariashklovska1991@gmail.com>
2017-08-31 16:43:31 +0300

    todoService/feature: service ready

--- src/app/todo/todo.controller.js
+++ src/app/todo/todo.controller.js
@@ -1,32 +1,66 @@
 import todoModule from './todo.module';
 import { URLS } from '../constants';
 import localStorageService from '../app.service';
+import todoService from './todo.service';
 
 export default todoModule
-    .controller('todoController', function listController($http, localStorageService) {
+    .controller('todoController', function todoController($http, localStorageService, todoService) {
         let self = this;
-        self.filterById = 1;
+        self.todo;
+        self.openInput = false;
+        self.newTitle = '';
+        let parentId = 1;
 
         self.filterData = function(item) {
-            if(item.listId === self.filterById){
+            if(item.listId === parentId){
                 return item;
             }
         };
 
-        if(localStorageService.get('todo')){
-            self.listGroups = localStorageService.get('todo');
-        }
-        else {
-            $http({ method: 'GET', url: URLS.todoURL })
-            .then(function successCallback(response) {
-                self.todo = response.data;
-                localStorageService.set('todo', self.todo);
-            })
-            .catch(function errorCallback() {
-                // self.todo = require('../../app-data/todo.json');
-                self.todo = [];
-            });
-        }
-              
+        self.saveTask = function() {
+            self.todo = todoService.create(self.newTitle, parentId);
+            localStorageService.set('todo', self.todo);
+            self.openInput = false;
+            self.newTitle = '';
+        };
+
+        self.deleteTask = function(id){
+            self.todo = todoService.delete(id);
+            localStorageService.set('todo', self.todo);
+        };
+
+        self.rewriteTask = function(id) {
+            self.openInput = true;
+            todoService.get(id);
+        };
+
+        self.changeTask = function(){
+            self.todo = todoService.update(self.newTitle);
+            localStorageService.set('todo', self.todo);
+            self.openInput = false;
+            self.newTitle = '';
+        };
+        
+
+        function InitPage(){
+            if(localStorageService.get('todo')){
+                self.todo = localStorageService.get('todo');
+                todoService.set(self.todo);
+            }
+            else {
+                $http({ method: 'GET', url: URLS.todoURL })
+                .then(function successCallback(response) {
+                    self.todo = response.data;
+                    localStorageService.set('todo', self.todo);
+                    todoService.set(self.todo);
+                })
+                .catch(function errorCallback() {
+                    // self.todo = require('../../app-data/todo.json');
+                    self.todo = [];
+                    todoService.set(self.todo);
+                });
+            }
+    }     
+    InitPage();   
 
     });
