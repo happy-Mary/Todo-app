e74cdcfb6fe0a0d7b19a4dda9e01f1fc44e0e4c4
happy-Mary <mariashklovska1991@gmail.com>
2017-08-31 14:49:54 +0300

    update

--- src/app/listgroup/listgroup.service.js
+++ src/app/listgroup/listgroup.service.js
@@ -3,11 +3,31 @@ import ListGroup from './listgroup.constructor';
 
 export default listGroupModule
 	.service('listGroupService', function(){
+		this.data =[];
+		var self = this;
+
+		function setListGroups(data) {
+      		self.data = data;
+   		}
+   		function getListGroups(){
+   			return data;
+   		}
+   		function getListGroup(id){
+   			self.data.forEach(function(group){
+   				if(group.id == id){
+   					console.log(group)
+   					return group;
+   				}
+   			})
+   		}
 		function createListGroup(name){
 			var a = new ListGroup(name);
 			console.log(a.id);
 		}
 		return{
-			createListGroup: createListGroup
+			set: setListGroups,
+			get: getListGroups,
+			create: createListGroup,
+			getGroup: getListGroup
 		}
 	});
