8adb56dc223f59b270e464bf112a9e0a5446bcec
Maria Shklovskaya <MariaShklovskaya@coherentsolutions.com>
2017-09-01 21:00:19 +0300

    markup-task/feature: page is ready

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -4,4 +4,15 @@ export default mainModule
     .controller('AppController', function AppController() {
         let self = this;
         self.hello = "HELLO, our TODO App started =)";
+        self.headerTitle = 'current list title';
+        self.marked = false;
+        self.taskFocused = false;
+        
+        self.focusAddTask = function(){
+            (self.taskFocused) ? self.taskFocused=false : self.taskFocused=true;
+            (self.taskFocused) ? document.querySelector(".newTaskTitle").focus() : document.querySelector(".newTaskTitle").blur();
+        };
+
+        
+        
     });
