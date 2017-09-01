78d69a6669b4666c0c0c2dc534bf70f31ab7726a
happy-Mary <mariashklovska1991@gmail.com>
2017-09-01 21:00:19 +0300

    markup-task/feature: page is ready

--- src/app/listgroup/listgroup.component.js
+++ src/app/listgroup/listgroup.component.js
@@ -2,9 +2,6 @@ import listGroupModule from './listgroup.module';
 
 export default listGroupModule
     .component('listGroupComponent',{
-        bindings: {
-            groupTitle: '@'
-        },
         template: require('./listgroup.templ.html'),
         controller: 'listGroupController'
     });
