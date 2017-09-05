d7f4891477ae591e801ab2c40c417791d3bee7fc
happy-Mary <mariashklovska1991@gmail.com>
2017-09-05 20:18:23 +0300

    marked-task/feature: active item menu

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -7,26 +7,29 @@ export default mainModule
         self.headerTitle = 'current list title';
         self.marked = false;
         self.taskFocused = false;
-        
-        var urlArray = $location.path().split('/');
-        self.currRoute = urlArray[urlArray.length-1];
-        // console.log(self.currRoute);
 
-      
-        
         self.focusAddTask = function(){
             (self.taskFocused) ? self.taskFocused=false : self.taskFocused=true;
             (self.taskFocused) ? document.querySelector(".newTaskTitle").focus() : document.querySelector(".newTaskTitle").blur();
         };
+        
+        var urlArray = $location.path().split('/');
+        self.currPath = urlArray[urlArray.length-1];
+        // console.log(self.currRoute);
+        console.log($location.path());
 
-        let marked = document.querySelector('.marked');
-        self.makeClick = function(){
-            document.querySelectorAll('li').forEach(function(item){
+        
+        // changing menu item 
+        self.changeActive = function($event){
+            let test = document.querySelector('.folders').querySelectorAll('li');
+            test.forEach(function(item){
+            
                 if(item.classList.contains('active-list')){
+                    console.log(item.$ctrl);
                     item.classList.remove('active-list');
                 }
             });
-            marked.classList.add('active-list');
+            $event.target.parentNode.classList.add('active-list');
         };
 
     });
