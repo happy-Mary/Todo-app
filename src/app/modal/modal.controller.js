0963a84852bfb13eca84fc6c01f6c18a4e21e299
MariaChernyak <mchernyak100@gmail.com>
2017-09-05 15:32:02 +0300

    update

--- src/app/modal/modal.controller.js
+++ src/app/modal/modal.controller.js
@@ -4,9 +4,10 @@ export default modalModule
     .controller('modalController', function modalController() {
         var self = this;
 
-        self.open =  false;
+        self.open = true;
         self.closeModal = function(){
         	self.open = false;
+        	console.log('sss')
         }
         self.openModal = function(){
         	self.open = true;
