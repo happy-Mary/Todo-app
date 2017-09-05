3826daf3cc2e9d4b58b1bc014b9dbfc9cc8bb29a
Maria Shklovskaya <MariaShklovskaya@coherentsolutions.com>
2017-09-05 19:35:54 +0300

    marked-task/feature: list menu

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,18 +1,32 @@
 import mainModule from './app.module';
 
 export default mainModule
-    .controller('AppController', function AppController() {
+    .controller('AppController', function AppController($location) {
         let self = this;
         self.hello = "HELLO, our TODO App started =)";
         self.headerTitle = 'current list title';
         self.marked = false;
         self.taskFocused = false;
         
+        var urlArray = $location.path().split('/');
+        self.currRoute = urlArray[urlArray.length-1];
+        // console.log(self.currRoute);
+
+      
+        
         self.focusAddTask = function(){
             (self.taskFocused) ? self.taskFocused=false : self.taskFocused=true;
             (self.taskFocused) ? document.querySelector(".newTaskTitle").focus() : document.querySelector(".newTaskTitle").blur();
         };
 
-        
-        
+        let marked = document.querySelector('.marked');
+        self.makeClick = function(){
+            document.querySelectorAll('li').forEach(function(item){
+                if(item.classList.contains('active-list')){
+                    item.classList.remove('active-list');
+                }
+            });
+            marked.classList.add('active-list');
+        };
+
     });
