fa9fd50931bbfb368a3c1054cf22b0c6396a3f02
happy-Mary <mariashklovska1991@gmail.com>
2017-09-07 19:41:27 +0300

    modal-task/feature: folder-component added

---
+++ src/app/folder-form/folderform.controller.js
@@ -0,0 +1,6 @@
+import folderFormModule from './folderform.module';
+
+export default folderFormModule
+.controller('folderFormController', function(modalService){
+    self.modal = modalService;
+});
