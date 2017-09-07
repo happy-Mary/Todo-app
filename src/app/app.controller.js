4a3fb1bff83ebb5beb0043f374305ff49ba7bf0a
MariaChernyak <mchernyak100@gmail.com>
2017-09-07 10:39:27 +0300

    add todo

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,7 +1,10 @@
 import mainModule from './app.module';
+import todoService from './todo/todo.service';
+import listService from './list/list.service';
+import localStorageService from './app.service';
 
 export default mainModule
-    .controller('AppController', function AppController() {
+    .controller('AppController', function AppController(todoService, $location, listService, localStorageService) {
         let self = this;
         self.hello = "HELLO, our TODO App started =)";
         self.headerTitle = 'current list title';
@@ -9,10 +12,32 @@ export default mainModule
         self.taskFocused = false;
         
         self.focusAddTask = function(){
-            (self.taskFocused) ? self.taskFocused=false : self.taskFocused=true;
+            (self.taskFocused) ? self.taskFocused = false : self.taskFocused = true;
             (self.taskFocused) ? document.querySelector(".newTaskTitle").focus() : document.querySelector(".newTaskTitle").blur();
         };
 
-        
-        
-    });
+        document.querySelector('main form').addEventListener('submit', function(event){
+            event.preventDefault();
+            document.querySelector('.newTaskTitle').blur();
+
+        });
+
+        self.addToDo = function(){
+            // event.preventDefault();
+             var input = angular.element(document.querySelector('.newTaskTitle'));
+             var newTodo = input.val().trim();
+            
+             if(newTodo){
+                console.log(newTodo);
+                var urlArray = $location.path().split('/');
+                var listId = urlArray[urlArray.length-1];
+                var todo = todoService.create(newTodo, listId);
+                localStorageService.set('todo', todoService.get());
+                listService.addTodo(listId, todo.id);
+                localStorageService.set('lists', listService.get());
+
+             }
+             input.val('');
+         }
+           
+    }); 
