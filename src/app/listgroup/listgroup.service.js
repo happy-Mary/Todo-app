f0c7c478b94074ef097c7ddc673271d29b64a605
happy-Mary <mariashklovska1991@gmail.com>
2017-09-08 13:14:58 +0300

    update services

--- src/app/listgroup/listgroup.service.js
+++ src/app/listgroup/listgroup.service.js
@@ -1,17 +1,40 @@
 import listGroupModule from './listgroup.module';
 import ListGroup from './listgroup.constructor';
+import { URLS } from '../constants';
+import localStorageService from '../app.service';
 
 export default listGroupModule
-	.service('listGroupService', function(){
+	.service('listGroupService', function($http, localStorageService){
 		this.data =[];
 		var self = this;
 
 		function setListGroups(data) {
-      		self.data = data;
+			self.data = data;
+			save();
+   		}
+   		function save(){
+   			localStorageService.set('listGroups', self.data);
    		}
    		function getListGroups(){
-   			return data;
-   		}
+   			if(localStorageService.get('listGroups')){
+          	  self.data = localStorageService.get('listGroups');
+          	  return self.data;
+            }
+	        else{
+	             $http({ method: 'GET', url: URLS.listGroupURL })
+	                .then(function successCallback(response) {
+	                    self.data = response.data;
+	                    save();
+	                    return self.data;
+	                 
+	                })
+	                .catch(function errorCallback() {
+	                   self.data =  [];
+	                   save();
+	                   return self.data;
+	                });
+	        }
+	    }
    		function getListGroup(id){
    			self.data.forEach(function(group){
    				if(group.id == id){
@@ -23,11 +46,13 @@ export default listGroupModule
    		function deleteListGroup(id) {
 	        var index = data.findIndex(group => group.id == id);
 	        self.data.splice(index, 1);
+	        save();
    		}
 
 		function createListGroup(name){
 			var group = new ListGroup(name);
 			self.data.push(data);
+			save();
 		}
 
 		return{
@@ -35,6 +60,7 @@ export default listGroupModule
 			get: getListGroups,
 			create: createListGroup,
 			getGroup: getListGroup,
-			deleteGroup: deleteListGroup
+			deleteGroup: deleteListGroup,
+			save: save
 		}
 	});
