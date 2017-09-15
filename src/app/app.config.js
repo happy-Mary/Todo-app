6084d641dfd3111601f0c121bd5986a94fc913a9
happy-Mary <mariashklovska1991@gmail.com>
2017-09-15 18:32:02 +0300

    router-task/feature: changes base-tag and locationProvider

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -4,7 +4,8 @@ import mainModule from './app.module';
 export default mainModule
 .config(['$locationProvider', '$routeProvider',
 function config($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  // $locationProvider.hashPrefix('!');
+  $locationProvider.html5Mode(true);
   $routeProvider
   .when('/lists/:listid', {
       template: '<todo-comp></todo-comp>',
