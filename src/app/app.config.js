c375f76b27a7367e407fb3b4117d7b3315b23a01
happy-Mary <mariashklovska1991@gmail.com>
2017-09-18 00:21:18 +0300

    router-task/feature: added stateProvider

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -2,19 +2,36 @@
 import mainModule from './app.module';
 
 export default mainModule
-.config(['$locationProvider', '$routeProvider',
-function config($locationProvider, $routeProvider) {
-  // $locationProvider.hashPrefix('!');
-  $locationProvider.html5Mode(true);
-  $routeProvider
-  .when('/lists/:listid', {
-      template: '<todo-comp></todo-comp>',
-      resolve: {
-        message: function($route, routeService) {
-          routeService.setlistid($route.current.params.listid);
-        }
-      }
-    })
-  .otherwise('lists/marked');
-}
+// .config(['$locationProvider', '$routeProvider',
+// function config($locationProvider, $routeProvider) {
+//   // $locationProvider.hashPrefix('!');
+//   $locationProvider.html5Mode(true);
+//   $routeProvider
+//   .when('/lists/:listid', {
+//       template: '<todo-comp></todo-comp>',
+//       resolve: {
+//         message: function($route, routeService) {
+//           routeService.setlistid($route.current.params.listid);
+//         }
+//       }
+//     })
+//   .otherwise('lists/marked');
+// }
+// ]);
+    .config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
+    function config($locationProvider, $stateProvider, $urlRouterProvider) {
+        // $locationProvider.hashPrefix('!');
+        $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise("/state1");
+        // $stateProvider
+        // .when('/lists/:listid', {
+        //     template: '<todo-comp></todo-comp>',
+        //     resolve: {
+        //         message: function($route, routeService) {
+        //             routeService.setlistid($route.current.params.listid);
+        //         }
+        //     }
+        // })
+        // .otherwise('lists/marked');
+    }
 ]);
