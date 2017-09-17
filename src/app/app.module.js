af252d2c6eab8c4cdaff1eaea640b9b6d10bdfcd
happy-Mary <mariashklovska1991@gmail.com>
2017-09-17 07:18:55 +0300

    router-task/feature: router

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,4 +1,3 @@
 import ngRoute from 'angular-route';
-// import uiRouter from 'angular-ui-router';
 
 export default angular.module('mainModule', ['ngRoute', 'listGroupModule', 'listModule', 'todoModule', 'modalModule']);
