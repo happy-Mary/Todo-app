6f6993584ba1b206985dd87706b868c923c6ec55
happy-Mary <mariashklovska1991@gmail.com>
2017-09-18 17:58:22 +0300

    master/feature: deleted ng-route

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,4 +1,3 @@
-import ngRoute from 'angular-route';
 import uiRouter from '@uirouter/angularjs';
 
-export default angular.module('mainModule', ['ngRoute', 'ui.router', 'listGroupModule', 'listModule', 'todoModule', 'modalModule']);
+export default angular.module('mainModule', ['ui.router', 'listGroupModule', 'listModule', 'todoModule', 'modalModule']);
