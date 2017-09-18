228fec2c50deff4036124a7c829f386fe4ac40cd
happy-Mary <mariashklovska1991@gmail.com>
2017-09-18 18:57:59 +0300

    router-task/feature: opening folder when list active

--- src/app/list/list.controller.js
+++ src/app/list/list.controller.js
@@ -11,7 +11,7 @@ export default listModule
         self.newListTitle = '';
      
         self.modal = modalService;
-        
+
         self.newList = function(title) {
             listService.create(title);
         };
