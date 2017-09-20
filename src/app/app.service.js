d1be581a61c89e9c5c7b9d2dcc59c3baa65774ba
MariaChernyak <mchernyak100@gmail.com>
2017-09-20 12:04:24 +0300

    update localStorage service and register functions

--- src/app/app.service.js
+++ src/app/app.service.js
@@ -3,11 +3,41 @@ import mainModule from './app.module';
 export default mainModule
 .service('localStorageService', function(){
 	function set(key, data){
-		localStorage.setItem(key, JSON.stringify(data));
+		let promise = new Promise(function(resolve, reject){
+			setTimeout(function(){
+				localStorage.setItem(key, JSON.stringify(data));
+				resolve();
+			}, 1000);
+		});
+		return promise;
 	}
 	function get(key){
-		return JSON.parse(localStorage.getItem(key));
+		let promise = new Promise(function(resolve, reject){
+			setTimeout(function(){
+				let resault = JSON.parse(localStorage.getItem(key));
+				if(resault){
+					resolve(resault);
+				}
+				else{
+					reject('Error');
+				}
+				
+			}, 1000);
+		});
+		return promise;
+
 	}
+	// function set(key, data){
+		
+	// 			localStorage.setItem(key, JSON.stringify(data));
+
+	// }
+	// function get(key){
+		
+	// 			let resault = JSON.parse(localStorage.getItem(key));
+	// 		return resault;
+
+	// }
 	return {
 		get: get,
 		set: set
