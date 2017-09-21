c461d57e402131f27b9579814a72cd2c3013834f
happy-Mary <mariashklovska1991@gmail.com>
2017-09-21 18:00:52 +0300

    update load data

--- src/app/listgroup/listgroup.controller.js
+++ src/app/listgroup/listgroup.controller.js
@@ -5,14 +5,8 @@ export default listGroupModule
     .controller('listGroupController', function listGroupController($http, localStorageService, listGroupService) {
         var self = this;
         self.listGroups = listGroupService.get();
-        // self.listGroups = listGroupService.data;
-        // console.log(self.listGroups)
-        // setTimeout(function(){
-        //     console.log('22222ssss');
-        //     console.log(self.listGroups)
-        // }, 3000)
         self.openFolder = function(event) {
-            let folderLink = angular.element(event.currentTarget);
+        let folderLink = angular.element(event.currentTarget);
             if (folderLink.hasClass('folder-item')) {
                 let folderItem = folderLink.parent();
                 (folderItem.hasClass('folder-close')) ? folderItem.removeClass('folder-close'): folderItem.addClass('folder-close');
