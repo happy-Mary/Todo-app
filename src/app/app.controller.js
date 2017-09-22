2bd5a5de579e86eebcef110cd87459610d4e7614
happy-Mary <mariashklovska1991@gmail.com>
2017-09-22 12:58:32 +0300

    completed-task/feature: fixed showing completed button on marked

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -26,33 +26,33 @@ export default mainModule
         listService.register();
         todoService.register();
 
-        angular.element(document).ready(function(event){
+        angular.element(document).ready(function(event) {
             // open folder if list active on first load
-             let activeList = angular.element(document.getElementsByClassName('active-list')[0]);
-             let targetEl = activeList.parent().parent().parent();
-             if(targetEl.hasClass('folder-close')){
-                 targetEl.removeClass('folder-close');
-             }
-             // clouse sort menu if mousleave/show if returns
-             let timeoutSortID;
-             sortMenuEl = angular.element(document.querySelector('.sort-menu'));
-             sortMenuEl.on('mouseleave', function() {
+            let activeList = angular.element(document.getElementsByClassName('active-list')[0]);
+            let targetEl = activeList.parent().parent().parent();
+            if (targetEl.hasClass('folder-close')) {
+                targetEl.removeClass('folder-close');
+            }
+            // clouse sort menu if mousleave/show if returns
+            let timeoutSortID;
+            sortMenuEl = angular.element(document.querySelector('.sort-menu'));
+            sortMenuEl.on('mouseleave', function() {
 
-                timeoutSortID = $timeout(function(){
+                timeoutSortID = $timeout(function() {
                     sortMenuEl.removeClass('sort-open');
                 }, 3000);
-              });
+            });
 
-              sortMenuEl.on('mouseenter', function(){            
-                  $timeout.cancel(timeoutSortID);
-              });
-         });
+            sortMenuEl.on('mouseenter', function() {
+                $timeout.cancel(timeoutSortID);
+            });
+        });
 
         // change main title on route
         $transitions.onSuccess({ to: 'lists.**' }, function(trans) {
             self.currListId = $stateParams.listid;
-           
-            if(self.currListId !== 'marked'){
+
+            if (self.currListId !== 'marked') {
                 let list = listService.getList(self.currListId);
 
                 // CHANGE AFTER RESOLVING GETTING DATA
@@ -64,8 +64,8 @@ export default mainModule
         });
 
         // redirect to search while typing
-        self.goToSearch = function(){
-            $state.go('lists.filter.search', {param: self.searchItem });
+        self.goToSearch = function() {
+            $state.go('lists.filter.search', { param: self.searchItem });
         };
 
         // focusing input for adding todo
@@ -74,56 +74,56 @@ export default mainModule
             document.querySelector(".newTaskTitle").focus();
         };
 
-       self.toggleFocus = function(event){
-            (self.taskFocused) ? self.taskFocused = false : self.taskFocused = true;
-            if(!self.taskFocused){
-               event.stopPropagation();
+        self.toggleFocus = function(event) {
+            (self.taskFocused) ? self.taskFocused = false: self.taskFocused = true;
+            if (!self.taskFocused) {
+                event.stopPropagation();
             }
         };
 
         // adding todo
-        self.addToDo = function(){
+        self.addToDo = function() {
             event.preventDefault();
             var newTodo = self.newTodoTitle.trim();
-            if(newTodo){
+            if (newTodo) {
                 let listId = $stateParams.listid;
                 todoService.create(newTodo, listId, self.marked);
             }
             self.newTodoTitle = '';
-         };
- 
+        };
+
         // functions for manipulating list, folders, ?todo? data
         self.activeItem = null;
 
         self.actions = {
             // editting item
-            onEdit: function(item) { 
+            onEdit: function(item) {
                 self.activeItem = item;
-                if(item.type === 'list') {
+                if (item.type === 'list') {
                     modalService.open('edit-list');
-                } else if(item.type === 'folder'){
+                } else if (item.type === 'folder') {
                     modalService.open('edit-folder');
                 }
             },
             // deleting item
             onDelete: function(item) {
                 self.activeItem = item;
-                 if(item.type === 'list') {
+                if (item.type === 'list') {
                     modalService.open('delete-list');
-                } else if(item.type === 'folder'){
+                } else if (item.type === 'folder') {
                     modalService.open('delete-folder');
                 }
             },
             // clicking on item
-            onActivate: function(){
+            onActivate: function() {
 
             },
             // opening folder
-            onToggleExpand: function(){
+            onToggleExpand: function() {
 
             },
             // opening folder menu (on custom right click)
-            onContextMenu: function(){
+            onContextMenu: function() {
 
             }
         };
@@ -131,38 +131,40 @@ export default mainModule
         // ////////////////////////////////
         // SORTING TODOS
         // ////////////////////////////////
-        self.toggleSortMenu = function(){
+        self.toggleSortMenu = function() {
             sortMenuEl.toggleClass('sort-open');
         };
-        
+
         let tasks = todoService.get();
-        console.log(tasks);
 
         self.sorting = {
-            byTitle: function(){
-                    tasks.sort(function(a, b) {
-                    let valueA=a.title.toLowerCase(), valueB=b.title.toLowerCase();
+            byTitle: function() {
+                tasks.sort(function(a, b) {
+                    let valueA = a.title.toLowerCase(),
+                        valueB = b.title.toLowerCase();
                     sortMenuEl.removeClass('sort-open');
                     return (valueA > valueB) ? 1 : (valueA < valueB) ? -1 : 0;
                 });
             },
 
             byMarked: function() {
-                    tasks.sort(function(a, b) {
-                    let valueA = -a.marked, valueB= -b.marked;
+                tasks.sort(function(a, b) {
+                    let valueA = -a.marked,
+                        valueB = -b.marked;
                     sortMenuEl.removeClass('sort-open');
                     return (valueA > valueB) ? 1 : (valueA < valueB) ? -1 : 0;
                 });
             },
             byDate: function() {
                 tasks.sort(function(a, b) {
-                    let valueA = new Date(a.date), valueB= new Date(b.date);
+                    let valueA = new Date(a.date),
+                        valueB = new Date(b.date);
                     sortMenuEl.removeClass('sort-open');
                     return (valueA > valueB) ? -1 : (valueA < valueB) ? 1 : 0;
                 });
 
                 // testing dates sorting (remove after tests)
-                angular.forEach(tasks, function(task){
+                angular.forEach(tasks, function(task) {
                     console.log(task.date);
                 });
                 /////////////////////////////////
@@ -171,4 +173,3 @@ export default mainModule
 
 
     });
-
