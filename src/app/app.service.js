c780df75eaea347b71ba92e72bc20d575fdec0cd
happy-Mary <mariashklovska1991@gmail.com>
2017-10-27 11:35:59 +0300

    server-task/feature: changing file structure

--- src/app/app.service.js
+++ src/app/app.service.js
@@ -1,7 +1,7 @@
 import mainModule from './app.module';
 
 export default mainModule
-    .service('localStorageService', ['$timeout', function localStorageService($http, $timeout) {
+    .service('localStorageService', ['$timeout', function localStorageService($timeout, $http) {
         function setData(key, data) {
             const promise = new Promise((resolve) => {
                 $timeout(() => {
@@ -30,34 +30,34 @@ export default mainModule
         }
 
         // //////////////////////////////////////////////////////////
-        function getData(url) {
-            const currUrl = url;
-            $http({ method: 'GET', url: currUrl }).then()
-        }
+        // function getData(url) {
+        //     const currUrl = url;
+        //     $http({ method: 'GET', url: currUrl }).then()
+        // }
 
-        function setData(url, data) {
-            const currUrl = url;
-            $http({ method: 'POST', url: currUrl, data: angular.toJson(data) }).then()
-        }
+        // function setData(url, data) {
+        //     const currUrl = url;
+        //     $http({ method: 'POST', url: currUrl, data: angular.toJson(data) }).then()
+        // }
 
-        function deleteData(url) {
-            // в сервисе, из которого вызываем метод, url = `${constUrl}/${elementId}`
-            const currUrl = url;
-            $http({ method: 'DELETE', url: currUrl }).then()
-        }
+        // function deleteData(url) {
+        //     // в сервисе, из которого вызываем метод, url = `${constUrl}/${elementId}`
+        //     const currUrl = url;
+        //     $http({ method: 'DELETE', url: currUrl }).then()
+        // }
 
-        function updateData(url, data) {
-            // в сервисе, из которого вызываем метод, url = `${constUrl}/${elementId}`
-            const currUrl = url;
-            $http({ method: 'UPDATE', url: currUrl, data: angular.toJson(data) }).then()
-        }
+        // function updateData(url, data) {
+        //     // в сервисе, из которого вызываем метод, url = `${constUrl}/${elementId}`
+        //     const currUrl = url;
+        //     $http({ method: 'UPDATE', url: currUrl, data: angular.toJson(data) }).then()
+        // }
 
         // ? как получать сабтаски и файлы, они будут возвращаться по parentId
 
         return {
             get: getData,
-            set: setData,
-            delete: deleteData,
-            update: updateData
+            set: setData
+            // delete: deleteData,
+            // update: updateData
         };
     }]);
