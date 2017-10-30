dfb80c589f6beeca019aeacbf1d62009331c0ebc
MariaChernyak <mchernyak100@gmail.com>
2017-10-30 14:38:45 +0300

    create autoheight directive

--- src/app/directives/autoheight.directive.js
+++ src/app/directives/autoheight.directive.js
@@ -1,8 +1,22 @@
 import mainModule from '../app.module';
 
 export default mainModule
-.directive('autoheight', () => {
-	 return function(scope, element, attrs) {
-	 
-	 };
- });
+.directive('autoheight', ($timeout) => {
+	return {
+	 	// function(scope, element, attrs) {
+	 	restrict: 'A',
+		link: function(scope, element, attrs) {	
+			element.ready(function() {
+		    	element.css('height', `${element[0].scrollHeight}px`);
+		    });
+		    
+		    element.on('keydown', (event) => {
+	        	let target = event.target;		        
+		        $timeout(() => {
+		         	element.css('height', 'auto');
+					element.css('height', `${target.scrollHeight}px`);
+				}, 0);
+	        });	
+		} 	
+	};
+});
