a44f9f2ea126a8c4006d436377193e790363c654
happy-Mary <mariashklovska1991@gmail.com>
2017-11-22 14:31:34 +0300

    master: checking location protocol and host

--- src/app/app.socket.js
+++ src/app/app.socket.js
@@ -2,9 +2,13 @@ import io from 'socket.io-client';
 import mainModule from './app.module';
 
 export default mainModule
-.factory('socket', ['socketFactory', function Socket(socketFactory) {
+.factory('socket', ['socketFactory', '$location', function Socket(socketFactory, $location) {
+    const url = 'https://wondrer-todoapp.herokuapp.com' || 'http://localhost:3000';
+    console.log($location.protocol());
+    console.log($location.host());
     return socketFactory({
         prefix: '',
         ioSocket: io.connect('http://localhost:3000')
+        // ioSocket: io.connect(url)
     });
   }])
