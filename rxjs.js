06529f791bc68db731f1d55860d5ad42ae5f9285
happy-Mary <mariashklovska1991@gmail.com>
2018-01-08 20:17:18 +0300

    async rxjs requests

--- rxjs.js
+++ rxjs.js
@@ -29,10 +29,93 @@ const Rx = require('rxjs');
 
 // example 2
 //emit array as a sequence of values
-const arraySource = Rx.Observable.from([1, 2, 3, 4, 5])
-.map((x) => x*2);
-//output: 1,2,3,4,5
-const subscribe = arraySource.subscribe(val => console.log(val));
-// /////////////
+// const arraySource = Rx.Observable.from([1, 2, 3, 4, 5])
+// .map((x) => x*2);
+// //output: 1,2,3,4,5
+// const subscribe = arraySource.subscribe(val => console.log(val));
+// // /////////////
+
+// example 3
+
+
+
+// const source = Rx.Observable.from([1, 2, 3, 4, 5]);
+
+// //mergeMap also emits result of promise
+// const myPromise = val =>
+//   new Promise((resolve) => {
+//     setTimeout(() => {
+//         let value = val +3;
+//         resolve(value);
+//     }, 2000);
+// }).then((item) => {
+//     console.log('first req');
+//     return item+2;
+// });
+
+// const  myPromise2 = val =>
+//   new Promise((resolve) => {
+//     setTimeout(() => {
+//         let value = val * 3;
+//         console.log('second req');
+//         resolve(value);
+//     }, 2000);
+// });
+
+// // const myPromise2 = val =>
+// // new Promise(resolve => resolve(val*3));
+// //map to promise and emit result
+// const example = source.take(3).concatMap(val => myPromise(val)).concatMap(val => myPromise2(val));
+// //output: 'Hello World From Promise'
+// const subscribe = example.subscribe(val => console.log(val));
 
 console.log('rxjs test');
+
+let t = new Promise((resolve) => {
+    setTimeout(() => {
+        let items = [
+            {name: 'jjj', age: 25},
+            {name: 'rrr', age: 4},
+            {name: 'sss', age: 12}
+        ];
+        resolve(items);
+    }, 2000)
+})
+.then((items)=> items);
+
+
+const source = Rx.Observable.fromPromise(t).flatMap(Rx.Observable.from);
+
+//mergeMap also emits result of promise
+const myPromise = val =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('first');
+        console.log(val.name);
+        resolve(val);
+    }, 2000);
+});
+
+const  myPromise2 = val =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('second req');
+        console.log(val);
+        resolve(val);
+    }, 2000);
+});
+
+
+
+// const myPromise2 = val =>
+// new Promise(resolve => resolve(val*3));
+//map to promise and emit result
+const example = source
+.map((val) => {
+    val.name = 'AAA';
+    return val;
+})
+.concatMap(val => myPromise(val))
+.concatMap(val => myPromise2(val));
+//output: 'Hello World From Promise'
+const subscribe = example.subscribe(val => console.log(`finish ${val.name}`));
